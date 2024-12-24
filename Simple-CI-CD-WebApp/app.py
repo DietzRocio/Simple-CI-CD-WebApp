6e8aeed1e830582c06598d7efb6473976dee1ddd
DietzRocio <76887031+DietzRocio@users.noreply.github.com>
2024-12-24 02:37:45 -0300

    Update app.py

--- Simple-CI-CD-WebApp/app.py
+++ Simple-CI-CD-WebApp/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Aquí puedes verificar si la variable de entorno PORT está definida
-port = os.getenv("PORT", 8080)  # Se usa 8080 como valor por defecto si no está configurado.
+port = os.getenv("PORT", )  # Se usa 8080 como valor por defecto si no está configurado.
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)  # Usamos el puerto obtenido de la variable de entorno.
