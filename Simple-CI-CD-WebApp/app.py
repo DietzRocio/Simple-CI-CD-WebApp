252f1687f9729c14dba215f90cab906ccd661d08
DietzRocio <76887031+DietzRocio@users.noreply.github.com>
2024-12-27 14:45:36 -0300

    Update app.py

--- Simple-CI-CD-WebApp/app.py
+++ Simple-CI-CD-WebApp/app.py
@@ -1,10 +1,19 @@
 import os
 from flask import Flask
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde un archivo .env si existe
+load_dotenv()
 
 app = Flask(__name__)
 
-# Aquí puedes verificar si la variable de entorno PORT está definida
-port = int(os.getenv("PORT", 8080))  # Asegúrate de que el puerto sea un entero
+# Obtener el puerto de la variable de entorno, con un valor por defecto de 8080
+port = int(os.getenv("PORT", 8080))
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port)  # Usamos el puerto obtenido de la variable de entorno.
+    app.run(host='0.0.0.0', port=port)
 
